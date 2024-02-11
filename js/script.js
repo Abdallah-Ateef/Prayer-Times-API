e5b27f75839f6768ee5bec8dd87e3e055945909c
Abdallah <abdallahatefelsayed@gmail.com>
2024-02-11 10:19:18 +0200

    Inital commit

---
+++ js/script.js
@@ -0,0 +1,91 @@
+
+function getprayertimes(city){
+    axios.get(`http://api.aladhan.com/v1/timingsByCity/:date?country=EG&city=${city}`)
+    .then((Response)=>{
+        let timings=Response.data.data.timings;
+        document.querySelector('.cards').innerHTML=`
+                    <div class="card">
+                    <div>
+                        <h2>الفجر</h2>
+                    </div>
+                    <div class="time">
+                        <h3>${timings.Fajr}</h3>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                    <div>
+                        <h2>الشروق</h2>
+                    </div>
+                    <div class="time">
+                        <h3>${timings.Sunrise}</h3>
+                    </div>
+                    
+                </div>
+            
+                <div class="card">
+                    <div>
+                        <h2>الظهر</h2>
+                    </div>
+                    <div class="time">
+                        <h3>${timings.Dhuhr}</h3>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                    <div>
+                        <h2>العصر</h2>
+                    </div>
+                    <div class="time">
+                        <h3>${timings.Asr}</h3>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                    <div>
+                        <h2>المغرب</h2>
+                    </div>
+                    <div class="time">
+                        <h3>${timings.Maghrib}</h3>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                    <div>
+                        <h2>العشاء</h2>
+                    </div>
+                    <div class="time">
+                        <h3>${timings.Isha}</h3>
+                    </div>
+                    
+                </div>
+        
+        `
+
+    });
+}
+
+getprayertimes('cairo')
+
+
+
+// let cities=document.querySelectorAll('select option');
+// cities.forEach(ele=>{
+//     ele.addEventListener('click',()=>{
+//         console.log(ele.value)
+//     })
+// })
+
+// let cities=document.querySelector('select');
+// cities.addEventListener('change',()=>{
+//     getprayertimes(cities.value);
+//     document.querySelector('.prayer-header h1').innerHTML=this.textContent;
+// });
+
+let cities=document.querySelectorAll('select option');
+cities.forEach((ele)=>{
+    ele.addEventListener('click',()=>{
+        getprayertimes(ele.value);
+        document.querySelector('.prayer-header h1').innerHTML=ele.textContent;
+    })
+})
