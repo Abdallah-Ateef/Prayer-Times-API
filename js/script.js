5e24d174cc93b0c425bc96829b43c631fa76507a
Abdallah <abdallahatefelsayed@gmail.com>
2024-02-11 11:55:20 +0200

    added some improvements

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@
 function getprayertimes(city){
     axios.get(`http://api.aladhan.com/v1/timingsByCity/:date?country=EG&city=${city}`)
     .then((Response)=>{
+        let date=Response.data.data.date.readable;
+        let weekday=Response.data.data.date.hijri.weekday.ar;
+        document.querySelector('.prayer-header h4').innerHTML=`${weekday} ${date}`;
+     
         let timings=Response.data.data.timings;
+        
         document.querySelector('.cards').innerHTML=`
                     <div class="card">
                     <div>
@@ -65,22 +70,10 @@ function getprayertimes(city){
     });
 }
 
+
 getprayertimes('cairo')
 
 
-
-// let cities=document.querySelectorAll('select option');
-// cities.forEach(ele=>{
-//     ele.addEventListener('click',()=>{
-//         console.log(ele.value)
-//     })
-// })
-
-// let cities=document.querySelector('select');
-// cities.addEventListener('change',()=>{
-//     getprayertimes(cities.value);
-//     document.querySelector('.prayer-header h1').innerHTML=this.textContent;
-// });
 
 let cities=document.querySelectorAll('select option');
 cities.forEach((ele)=>{
